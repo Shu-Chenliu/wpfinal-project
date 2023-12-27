01ac29045cf33c6432766ec838c7a9a7b66f70a5
susan <h811124@nehs.hc.edu.tw>
2023-12-27 13:03:55 +0800

    navbar

--- src/app/homepage/Navbar.tsx
+++ src/app/homepage/Navbar.tsx
@@ -1,4 +1,4 @@
-import { RxAvatar } from "react-icons/rx";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { UserRound } from 'lucide-react';
@@ -23,7 +23,7 @@ async function Navbar() {
   const username=session.user.username;
   return (
     <nav className="flex w-full flex-col bg-slate-100 pb-10 bg-slate-900">
-      <nav className="sticky top-0 flex flex-col items-center justify-between border-b bg-slate-800 pb-2">
+      <nav className="sticky top-0 flex flex-col items-center justify-between border-b bg-slate-800 pb-2 ">
         <div className="flex w-full items-center justify-between px-3 py-1">
           <div className="flex items-center gap-2">
             {/* <RxAvatar  /> */}
@@ -37,7 +37,7 @@ async function Navbar() {
         
           <Link href={"/homepage"}>
             <Button
-              className=" w-full my-2 hover:bg-orange-500 m-2"
+              className=" my-2 hover:bg-orange-500 m-2"
             >
               <Eye className="m-2" /> View All products
             </Button>
@@ -45,45 +45,45 @@ async function Navbar() {
 
           <Link href={"/homepage/ShoppingCart"}>
             <Button
-            className="flex my-2 w-full hover:bg-orange-500 m-2">
+            className="flex my-2  hover:bg-orange-500 m-2">
               <ShoppingCart className="m-2" /> Shopping Cart
             </Button>
           </Link>
 
           <Link href={"/homepage/MyMarket"}>
             <Button
-            className="flex my-2 w-full hover:bg-orange-500 m-2">
+            className="flex my-2 hover:bg-orange-500 m-2">
               <Store className="m-2" /> My market
             </Button>
           </Link>
           <Link href={"/homepage/SellerNotifications"}>
             <Button
-            className="flex w-full my-2 hover:bg-orange-500 m-2"
+            className="flex my-2 hover:bg-orange-500 m-2"
             ><BellPlus className="m-2" />Market Notifications </Button>
           </Link>
         
 
           <Link href={"/homepage/BuyerNotifications"}>
             <Button
-            className="flex w-full my-2 hover:bg-orange-500 m-2"
+            className="flex  my-2 hover:bg-orange-500 m-2"
             ><BellRing className="m-2" />Buying Notifications</Button>
           </Link>
           <Link href={"/homepage/getCoupon"}>
             <Button
-            className="flex w-full my-2 hover:bg-orange-500 m-2"
+            className="flex my-2 hover:bg-orange-500 m-2"
             ><Ticket className="m-2"/>
               Get Coupon</Button>
           </Link> 
 
           <Link href={"/homepage/Chat"}>
             <Button
-            className="flex w-full my-2 hover:bg-orange-500 m-2"
+            className="flex my-2 hover:bg-orange-500 m-2"
             ><MessagesSquare className="m-2" />Chatroom</Button>
           </Link> 
 
           <Link href={"/homepage/MyAccount"}>
             <Button
-            className="flex w-full my-2 hover:bg-orange-500 m-2"
+            className="flex  my-2 hover:bg-orange-500 m-2"
             ><UserRound className="m-2" />My account</Button>
           </Link> 
           
@@ -91,7 +91,7 @@ async function Navbar() {
             <Button
               // variant={"ghost"}
               type={"submit"}
-              className="w-full hover:bg-orange-500 m-2"
+              className=" hover:bg-orange-500 m-2"
             >
               <LogOut className="m-2"/>
               Sign Out

fbf875d263d2dfd220f78b269b46fa021dd25b90
susan <h811124@nehs.hc.edu.tw>
2024-07-18 10:52:48 +0800

    add alert when signout

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,23 +1,31 @@
 "use client";
 
 import { useEffect } from "react";
-
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
-import { publicEnv } from "@/lib/env/public";
 
 function SignOutPage() {
   const { data: session } = useSession();
   const router = useRouter();
+
   useEffect(() => {
     if (session) {
-      signOut({ callbackUrl: publicEnv.NEXT_PUBLIC_BASE_URL });
+      signOut()
+        .then(() => {
+          alert("Signed out successfully");
+          router.push("/");
+        })
+        .catch((error) => {
+          alert("Error during sign out: " + error.message);
+        });
+    } else {
+      alert("No session found, redirecting to home page");
+      router.push("/");
     }
-    router.push("/");
   }, [session, router]);
 
   return <></>;
 }
 
 export default SignOutPage;
+
